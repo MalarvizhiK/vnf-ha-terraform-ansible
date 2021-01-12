3fec40c91c4935ac1a6d24272bbc1424832fd384
MalarvizhiK <k.malarvizhi@in.ibm.com>
2021-01-12 14:54:12 +0530

    Delete update_script.sh

--- script/update_script.sh
+++
@@ -1,9 +0,0 @@
-ip_address="$1"
-
-echo "Updating tgactive script ..."
-
-if ! grep -q $ip_address "/config/failover/tgactive"; then
-  echo curl http://$ip_address:3000/ >> /config/failover/tgactive
-fi
- 
-echo "Updated tgactive with url http://$ip_address:3000/" 
